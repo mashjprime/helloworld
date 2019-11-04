1681f5a1d0572a9614fcb7fcaa5f710c1c924f61
Mash J Prime <mashjprime@gmail.com>
2019-11-04 16:53:59 +0800

    My first variable.. Hahahaha

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,6 +8,9 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is my first C# application ;)");
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"You are awesome {name}");
         }
     }
 }
