3ee622d5250978f04bf160a7de7390a7fca15139
Mash J Prime <mashjprime@gmail.com>
2019-11-04 20:30:07 +0800

    Splitting arrays

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,12 +7,21 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int[] a = { -5, 2, 123, 56, 3 };
+            int[] a = { -5, 2, 123, 56, (int)3.5 };
             for (int i = 0; i < a.Length; i++)
             {
                 Console.WriteLine(a[i]);
             }
 
+            Console.WriteLine("Hey, would you like a \ttab?");
+            String splitMe = "These,values,are,comma,separated";
+            string[] intoArray = splitMe.Split(',');
+            int j = 0;
+            while(j<intoArray.Length)
+            {
+                Console.Write(intoArray[j]+ " ");
+                j++;
+            }
 
         }
     }
