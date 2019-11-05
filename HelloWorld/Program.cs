9b63286cb785f8e60681dd7d298fd69ad9f6babf
Mash J Prime <mashjprime@gmail.com>
2019-11-06 04:34:28 +0800

    File reading code complete with comments

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -18,15 +18,19 @@ namespace HelloWorld
 
         static void textFiles()
         {
+            // save the file path as a string variable
             string filePath = @"D:\code\c-sharp\HelloWorld\HelloWorld\data.txt";
-            
-            List<Person> people = new List<Person>();
-            List<string> lines = File.ReadAllLines(filePath).ToList();
 
+            // create person object array
+            List<Person> people = new List<Person>();
+            // create text file line array
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            // split each word at the , and save it into an array
             foreach (string line in lines)
             {
                 string[] entries = line.Split(',');
 
+                // Create person object and populate the data
                 Person newPerson = new Person();
 
                 newPerson.FirstName = entries[0];
@@ -34,25 +38,26 @@ namespace HelloWorld
                 newPerson.LastName = entries[2];
                 newPerson.Age = entries[3];
                 newPerson.MoreInfo = entries[4];
-
+                // Add person object to people array
                 people.Add(newPerson);
             }
-
+            // List all the objects in people array to console
             foreach (var person in people)
             {
                 Console.WriteLine($" {person.FirstName} {person.MiddleName} {person.LastName}: {person.Age}");
                 Console.WriteLine("\t^" + person.MoreInfo);
             }
-
+            // Add a new person object to people array
             people.Add(new Person { FirstName = "Fish", MiddleName = "in", LastName = "tank", Age = "2", MoreInfo = "So purdy" });
-
+            // Create new array for storing people data in a csv exportable format
             List<string> output = new List<string>();
             foreach (var person in people)
             {
+                // This is the comma seperated format
                 output.Add($"{person.FirstName},{person.MiddleName},{person.LastName},{person.Age},{person.MoreInfo}");
             }
             Console.WriteLine("Writing to file...");
-
+            // Overwrite text file with new data
             File.WriteAllLines(filePath, output);
 
             Console.WriteLine("Complete");
